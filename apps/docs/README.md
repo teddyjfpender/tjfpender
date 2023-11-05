bd38e97051a56c228ca6bef0d1ffba03a5eedf73
teddyjfpender <teddyjfpender@gmail.com>
2023-11-05 20:02:29 +0000

    init repo

---
+++ apps/docs/README.md
@@ -0,0 +1,11 @@
+# My Blog
+
+## Local Development
+
+First, run `pnpm i` to install the dependencies.
+
+Then, run `pnpm dev` to start the development server and visit localhost:3000.
+
+## License
+
+This project is licensed under the Apache 2.0 License.
---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router';
+
+
+
+const config: DocsThemeConfig = {
+	logo: <span>Teddy Pender Blog</span>,
+	project: {
+		link: "https://github.com/palladians/herald",
+	},
+	chat: {
+		link: "https://discord.com/channels/1127906495409958953/1137446548469448805",
+	},
+	docsRepositoryBase: "https://github.com/palladians/herald/tree/main/apps/docs",
+	footer: {
+		text: (
+			<span>Apache 2.0 {new Date().getFullYear()} © Teddy Pender</span>
+		),
+	},
+	editLink: {
+		text: (
+			<span>TP Blog | Edit on GitHub</span>
+		),
+	},
+	primaryHue: {light: 45, dark: 45},
+	useNextSeoProps() {
+		const { asPath } = useRouter();
+		if (asPath !== "/") {
+			return {
+				titleTemplate: "%s",
+			};
+		}
+	},
+	themeSwitch: {
+		useOptions() {
+			return {
+				light: "Light",
+				dark: "Dark",
+				system: "System",
+			};
+		},
+	},
+};
+
+export default config
